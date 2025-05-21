8533d5dca2bf75cf108431162aab56e1fc2cf737
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2025-05-20 21:25:52 -0400

    server.PlaidItem doesn't need JsonCodec

--- jvm/src/main/scala/maasertracker/server/PlaidItem.scala
+++ jvm/src/main/scala/maasertracker/server/PlaidItem.scala
@@ -1,9 +1,7 @@
 package maasertracker.server
 
-import io.circe.generic.JsonCodec
 import maasertracker.Institution
 
-@JsonCodec
 case class PlaidItem(itemId: String, accessToken: String, institution: Institution) {
   def toShared = maasertracker.PlaidItem(itemId = itemId, institution = institution)
 }
